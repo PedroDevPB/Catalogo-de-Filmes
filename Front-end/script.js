ae8fa47fd19c7dca845c632f41451216c157501e
4rthurdev <arthur.silva@maisunifacisa.com.br>
2025-05-15 13:04:02 -0300

    Add categorias

--- Front-end/script.js
+++ Front-end/script.js
@@ -1,3 +1,4 @@
+// script para carregar o JSON e exibir os filmes
 fetch('filmes.json')
   .then(res => res.json())
   .then(filmes => {
@@ -10,6 +11,23 @@ fetch('filmes.json')
         <img src="${filme.imagem}" alt="${filme.titulo}">
         <h3>${filme.titulo}</h3>
         <p>${filme.descricao}</p>
+      `;
+      container.appendChild(card);
+    });
+  });
+
+// script para carregar o JSON e exibir as categorias
+fetch('categorias.json')
+  .then(res => res.json())
+  .then(categorias => {
+    const container = document.getElementById('categorias');
+    categorias.forEach(categoria => {
+      const card = document.createElement('div');
+      card.classList.add('card');
+
+      card.innerHTML = `
+        <h3>${categoria.nome}</h3>
+        <p>${categoria.descricao}</p>
       `;
       container.appendChild(card);
     });
